fc38e2cd444b8d7e9406fac642242d95c03de2f0
MinyShrimp <ksk7584@gmail.com>
2023-02-15 16:39:21 +0900

    6강: 로그인 처리 1 - 쿠키, 세션 - 로그인 처리하기 - 서블릿 HTTP 세션 2 - SessionAttribute

--- src/main/java/hello/springcoremvc26/web/HomeController.java
+++ src/main/java/hello/springcoremvc26/web/HomeController.java
@@ -1,32 +1,21 @@
 package hello.springcoremvc26.web;
 
 import hello.springcoremvc26.domain.member.Member;
-import hello.springcoremvc26.web.session.SessionManager;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
-    private final SessionManager sessionManager;
-
     @GetMapping("/")
     public String homeLogin(
-            HttpServletRequest req,
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
             Model model
     ) {
         // 세션이 없으면 home
-        HttpSession session = req.getSession(false);
-        if (session == null) {
-            return "home";
-        }
-
-        // 로그인
-        Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
         if (loginMember == null) {
             return "home";
         }
